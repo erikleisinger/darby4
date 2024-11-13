2ef16edd29f7d4f464805a2673d2460e96de9bfd
erikleisinger <erikleisinger@gmail.com>
2024-11-13 07:48:13 -0500

    add missing charaacter

--- src/pages/Home/ui/Bio.vue
+++ src/pages/Home/ui/Bio.vue
@@ -1,32 +1,35 @@
 <template>
-<div class="relative overflow-hidden">
-  <MainSection class="relative z-10 -mt-[1rem] lg:-mt-[2rem] 2xl:max-w-[100ch] m-auto">
+  <div class="relative overflow-hidden">
+    <MainSection
+      class="relative z-10 -mt-[1rem] lg:-mt-[2rem] 2xl:max-w-[100ch] m-auto"
+    >
+      <template #title>Bio</template>
+      <div class="rounded-md p-4 bg-black/40">
+        I’m Darby Yule, a Digital and User Experience Designer based in Burnaby,
+        BC. I specialize in crafting user-centred designs that help small
+        businesses and tech entrepreneurs succeed in a competitive landscape. My
+        approach is grounded in authenticity and collaboration, ensuring that
+        every project reflects the unique voice of my clients.
+        <br />
+        <br />
+        With a diverse background in music, content creation, and
+        entrepreneurship, I provide high-quality, project-based design services
+        tailored to the needs of startups, NGOs, and SaaS companies. My
+        data-driven approach ensures that designs not only look great but also
+        meet your business goals.
 
-    <template #title>Bio</template>
-    <div class="rounded-md p-4 bg-black/40  ">
-      I’m Darby Yule, a Digital and User Experience Designer based in Burnaby, BC.
-      I specialize in crafting user-centred designs that help small businesses and tech entrepreneurs succeed in a
-      competitive landscape.
-      My approach is grounded in authenticity and collaboration, ensuring that every project reflects the unique voice
-      of my
-      clients.
-      <br /> <br />
-      With a diverse background in music, content creation, and entrepreneurship, I provide high-quality, project-based
-      design services tailored to the needs of startups, NGOs, and SaaS companies.
-      My data-driven approach ensures that designs not only look great but also meet your business goals.
+        <br />
+        <br />
 
-      <br /> <br />
-
-      believe that design should be simple yet impactful. By prioritizing clarity and meaningful experiences, I help
-      businesses connect with their audience on a deeper level.
-      If you’re ready to elevate your brand and create exceptional digital experiences, let’s connect and explore how we
-      can
-      bring your vision to life!
-    </div>
-
-  </MainSection>
-  <div class="absolute inset-0 bg" />
-</div>
+        I believe that design should be simple yet impactful. By prioritizing
+        clarity and meaningful experiences, I help businesses connect with their
+        audience on a deeper level. If you’re ready to elevate your brand and
+        create exceptional digital experiences, let’s connect and explore how we
+        can bring your vision to life!
+      </div>
+    </MainSection>
+    <div class="absolute inset-0 bg" />
+  </div>
 </template>
 <style lang="scss" scoped>
 .bg {
@@ -40,5 +43,5 @@
 }
 </style>
 <script setup lang="ts">
-import { MainSection } from '@/shared/MainSection';
+import { MainSection } from '@/shared/MainSection'
 </script>
