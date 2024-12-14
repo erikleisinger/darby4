4a517857399ef897ca6b33416e9c5406ccc91a2a
erikleisinger <erikleisinger@gmail.com>
2024-12-14 09:44:14 -0500

    add analytics

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { INIT_ANALYTICS } from '@/shared/utils/analytics'
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
 import Layout from '@/app/Layout.vue'
+
+onMounted(() => {
+  INIT_ANALYTICS()
+})
 </script>
 
 <template>
-<Layout>
-  <RouterView />
-</Layout>
+  <Layout>
+    <RouterView />
+  </Layout>
 </template>
